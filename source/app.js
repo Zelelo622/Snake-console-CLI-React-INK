05f79da762f37058e51ca180fd05387bdca61d1c
Zelelo622 <egorcom27@gmail.com>
2026-01-14 10:16:26 +0300

    feat: display apple and start snake

--- source/app.js
+++ source/app.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, Box, useApp} from 'ink';
 
 const FIELD_SIZE = 16;
 const FIELD_ROW = [...new Array(FIELD_SIZE).keys()];
 
+const foodItem = {
+	x: Math.round(Math.random() * FIELD_SIZE),
+	y: Math.round(Math.random() * FIELD_SIZE),
+};
+
+function getItem(x, y, snakeSegments) {
+	if (x === foodItem.x && y === foodItem.y) {
+		return <Text>🍎 </Text>;
+	}
+
+	for (const segment of snakeSegments) {
+		if (x === segment.x && y === segment.y) {
+			return <Text color="green"> ◼ </Text>;
+		}
+	}
+}
+
 export default function App() {
 	const {exit} = useApp();
+	const [snakeSegments, setSnakeSegments] = useState([
+		{x: 8, y: 8},
+		{x: 8, y: 7},
+		{x: 8, y: 6},
+	]);
 
 	return (
 		<Box flexDirection="column" alignItems="center">
@@ -16,7 +38,7 @@ export default function App() {
 				{FIELD_ROW.map(y => (
 					<Box key={y}>
 						{FIELD_ROW.map(x => (
-							<Text key={x}> . </Text>
+							<Text key={x}>{getItem(x, y, snakeSegments) || ' . '}</Text>
 						))}
 					</Box>
 				))}
